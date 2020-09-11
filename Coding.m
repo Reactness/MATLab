dae552f0addca706bda82538399193915476931b
Denis <43929555+Teselekta@users.noreply.github.com>
2020-09-11 18:47:04 +0300

    Added my lab's

---
+++ Coding.m
@@ -0,0 +1,6 @@
+%Algorithm of Encryption
+% Examples of use:
+% >>Coding
+% Copyright Denis Zakorko 15.12.2019
+Text='Masha kasha';
+CodedWord=char(Text+1)
---
+++ Decoding.m
@@ -0,0 +1,5 @@
+%Algorithm of Decryption
+% Examples of use:
+% >>DeCoding
+% Copyright Denis Zakorko 15.12.2019
+DeCodedWord=char(CodedWord -1)
